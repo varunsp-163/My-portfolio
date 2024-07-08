edfd8b72431d88c36b715576e2a2cdf26dc2f446
varunsp-163 <spvarun47@gmail.com>
2024-07-08 14:36:49 +0530

    init

--- pages/leetcode.jsx
+++ pages/leetcode.jsx
@@ -54,11 +54,12 @@ const Leetcode = () => {
         setUserDetails(profileRes.data);
         setBadges(badgesRes.data.badges);
         setContestDetails(contestRes.data);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      } finally {
         setLoadingProfile(false);
         setLoadingBadges(false);
         setLoadingContest(false);
-      } catch (err) {
-        console.error("Error fetching data:", err);
       }
     };
 
@@ -78,8 +79,8 @@ const Leetcode = () => {
       {
         label: "Contest Rankings",
         data: contestRankings,
-        borderColor: "rgba(75,192,192,1)",
-        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(255,165,0,1)", // Changed to orange
+        backgroundColor: "rgba(255,165,0,0.2)", // Changed to orange
         fill: true,
         tension: 0.1,
       },
@@ -87,92 +88,91 @@ const Leetcode = () => {
   };
 
   return (
-    <div className="flex text-white flex-col items-center p-4">
-      <div className="w-full max-w-2xl">
-        <div className="p-6 rounded-lg  mb-6 flex items-center">
-          {loadingProfile ? (
-            <FadeLoader color={"#36D7B7"} loading={loadingProfile} />
-          ) : (
-            <img
-              src={userDetails.avatar}
-              alt="User Avatar"
-              className="w-24 h-24 rounded-full mx-auto md:mx-0 shadow-md"
-            />
+    <div className="flex flex-col items-center w-full space-y-6">
+      <div className="bg-white  rounded-lg shadow-lg flex items-center space-x-6">
+        {loadingProfile ? (
+          <FadeLoader color={"#36D7B7"} loading={loadingProfile} />
+        ) : (
+          <img
+            src={userDetails.avatar}
+            alt="User Avatar"
+            className="w-24 h-24 rounded-full shadow-md"
+          />
+        )}
+        <div className="text-center md:text-left">
+          {userDetails.name && (
+            <h2 className="text-2xl font-bold text-gray-800">
+              {userDetails.name}
+            </h2>
           )}
-          <div className="md:ml-6 text-center md:text-left">
-            {userDetails.name && (
-              <h2 className="text-2xl font-bold ">{userDetails.name}</h2>
-            )}
-            {userDetails.birthday && (
-              <p className="">Birthday: {userDetails.birthday}</p>
-            )}
-            {userDetails.ranking !== null && (
-              <p className="">Ranking: {userDetails.ranking}</p>
-            )}
-          </div>
+          {userDetails.birthday && (
+            <p className="text-gray-600">Birthday: {userDetails.birthday}</p>
+          )}
+          {userDetails.ranking !== null && (
+            <p className="text-gray-600">Ranking: {userDetails.ranking}</p>
+          )}
         </div>
-
-        {loadingContest ? (
-          <FadeLoader color={"#36D7B7"} loading={loadingContest} />
-        ) : (
-          contestDetails.contestParticipation.length > 0 && (
-            <div className=" p-6 rounded-lg  mb-6">
-              <h3 className="text-xl font-semibold ">Contest Rankings Graph</h3>
-              <Line data={chartData} />
-            </div>
-          )
-        )}
-
-        <div className=" p-6 rounded-lg  mb-6">
-          <h3 className="text-xl font-semibold ">Contest Details</h3>
-          <p className=" mt-2">Attended: {contestDetails.contestAttend}</p>
-          <p className=" mt-2">
-            Rating: {contestDetails.contestRating.toFixed(2)}
-          </p>
-          <p className=" mt-2">
-            Global Ranking: {contestDetails.contestGlobalRanking}
-          </p>
-          <p className=" mt-2">
-            Total Participants: {contestDetails.totalParticipants}
-          </p>
-          <p className=" mt-2">
-            Top Percentage: {contestDetails.contestTopPercentage.toFixed(2)}%
-          </p>
-        </div>
-
-        {loadingBadges ? (
-          <FadeLoader color={"#36D7B7"} loading={loadingBadges} />
-        ) : (
-          badges.length > 0 && (
-            <div className=" p-6 rounded-lg  mb-6">
-              <h3 className="text-xl font-semibold ">Badges</h3>
-              <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-4">
-                {badges.map((badge) => (
-                  <div key={badge.id} className="flex flex-col">
-                    {badge.id == 4183371 ? (
-                      <img
-                        src={
-                          "https://leetcode.com/static/images/badges/dcc-2024-6.png"
-                        }
-                        alt={badge.displayName}
-                        className="w-16 h-16"
-                      />
-                    ) : (
-                      <img
-                        src={badge.icon}
-                        alt={badge.displayName}
-                        className="w-16 h-16"
-                      />
-                    )}
-
-                    <p className=" mt-2 text-sm">{badge.displayName}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )
-        )}
       </div>
+
+      {loadingContest ? (
+        <FadeLoader color={"#36D7B7"} loading={loadingContest} />
+      ) : (
+        contestDetails.contestParticipation.length > 0 && (
+          <div className="bg-white  rounded-lg shadow-lg">
+            <h3 className="text-xl font-semibold text-gray-800">
+              Contest Rankings Graph
+            </h3>
+            <Line data={chartData} />
+          </div>
+        )
+      )}
+
+      <div className="bg-white  rounded-lg shadow-lg">
+        <h3 className="text-xl font-semibold text-gray-800">Contest Details</h3>
+        <p className="text-gray-600 mt-2">
+          Attended: {contestDetails.contestAttend}
+        </p>
+        <p className="text-gray-600 mt-2">
+          Rating: {contestDetails.contestRating.toFixed(2)}
+        </p>
+        <p className="text-gray-600 mt-2">
+          Global Ranking: {contestDetails.contestGlobalRanking}
+        </p>
+        <p className="text-gray-600 mt-2">
+          Total Participants: {contestDetails.totalParticipants}
+        </p>
+        <p className="text-gray-600 mt-2">
+          Top Percentage: {contestDetails.contestTopPercentage.toFixed(2)}%
+        </p>
+      </div>
+
+      {loadingBadges ? (
+        <FadeLoader color={"#36D7B7"} loading={loadingBadges} />
+      ) : (
+        badges.length > 0 && (
+          <div className="bg-white  rounded-lg shadow-lg">
+            <h3 className="text-xl font-semibold text-gray-800">Badges</h3>
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 mt-4">
+              {badges.map((badge) => (
+                <div key={badge.id} className="flex flex-col items-center">
+                  <img
+                    src={
+                      badge.id === 4183371
+                        ? "https://leetcode.com/static/images/badges/dcc-2024-6.png"
+                        : badge.icon
+                    }
+                    alt={badge.displayName}
+                    className="w-16 h-16"
+                  />
+                  <p className="text-gray-600 mt-2 text-sm">
+                    {badge.displayName}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 };
