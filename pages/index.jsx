fd9d3298f44fd3162c95a809c2dc96f372009654
varunsp-163 <spvarun47@gmail.com>
2024-07-29 21:53:02 +0530

    Added Vercel Web Analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Illustration from "../components/Illustration";
 import styles from "../styles/HomePage.module.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function HomePage() {
   return (
@@ -14,18 +15,15 @@ export default function HomePage() {
         <div className={styles.foreground}>
           <div className={styles.content}>
             <h1 className={styles.name}>Varun S P</h1>
-            <h6 className={styles.bio}>Frontend Web Developer
-            
-            </h6>
+            <h6 className={styles.bio}>Frontend Web Developer</h6>
+            <Analytics />
 
             <Link href="/projects">
               <button className={styles.button}>View Work</button>
             </Link>
             <Link href="/contact">
               <button className={styles.outlined}>Contact Me</button>
-              
             </Link>
-            
           </div>
         </div>
       </div>
